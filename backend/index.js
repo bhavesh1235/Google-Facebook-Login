66963f03e13b0f153a76274c95271e9d52bdd010
bhavesh1235 <bhavesh@elgaroma.com>
2021-12-11 23:38:08 +0530

    env file addedd

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,17 @@ app.use(
 
 app.use("/auth", authRoute);
 
-app.listen("5000", () => {
+const PORT = process.env.PORT || 5000; 
+
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+  const path=require('path');
+  app.get('*',(req,res)=>{
+      res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'))
+  })
+}
+
+app.listen(PORT , () => {
   console.log("Server is running!");
 });
